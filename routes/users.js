aa33827987c49491aeb24cbef55ff413318d628c
paul <tuner777@yahoo.com>
2018-06-04 11:37:18 -0500

    added register method

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
-var bodyParser = require('body-parser');
-
-
 var passport = require('passport');
-router.get('/', function(req, res, next) {
-  
-  res.render('users/index', { title: 'Create an Account' });
-});
-router.get('/login', function(req, res, next){
-  res.render('users/login');
-});
-router.post('/login',
-passport.authenticate('local'),
-function(req, res, next){
-  res.redirect('ionic/');
+var User = require('../models/user');
+
+router.get('/', function(req, res, next){
+  res.render('users/index', {title: 'User Management'});
 });
 
-module.exports = router
+router.get('/register', function(req, res, next) {
+  res.render('users/register', { 
+    title: 'Create an Account'
+   });
+});
+
+router.get('/login', function(req, res, next){
+  res.render('users/login', {title: 'Login'});
+});
+
+router.post(
+  '/login',
+  passport.authenticate('local'), 
+  function(req, res, next){
+    res.redirect('/ionic');
+});
+
+router.get('/logout', function(req, res, next){
+  req.logout();
+  res.redirect('/users/login');
+});
+
+module.exports = router;
